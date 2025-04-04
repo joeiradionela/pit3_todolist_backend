891b995181c6961d3838be3198bb57e9d10fb9b3
joeiradionela <joeiradionela@gmail.com>
2025-04-04 10:00:01 +0800

    update changes

--- todolist_backend/settings.py
+++ todolist_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ajgcbb2@-@nks7s00g@cc^toaiarbgb73k^^)=#w64dsq6=ggr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["pit3-todolist-backend.onrender.com"]
 
 
 # Application definition
